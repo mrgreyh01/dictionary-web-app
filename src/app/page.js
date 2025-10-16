1a7f3fddcc0d91ab2cd43faf9e56ef8eee3f2671
govpk00 <govpk00@gmail.com>
2025-10-16 22:42:50 +0530

    design is responsive for all the devices

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,205 @@
 import Image from "next/image";
+import { MouseEventHandler } from "react";
+
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="flex flex-col w-full h-full min-w-[365px] min-h-[1065] max-w-[1088px] max-h-[1205px] p-6 gap-8">
+        <Header />
+        <Searchbar />
+        <Search_keyword />
+        <Noun />
+        <Verb />
+        <Source />
+      </div>
+    </div>
+    
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+// header component
+function Header() {
+  return (
+    <div className="flex items-center justify-between gap-20  text-black md:pt-8">
+      <img className="h-fit w-fit font-bold" src="images/logo.svg" alt="" />
+      <div className="flex items-center justify-center gap-4">
+        <Dropdown />
+        <div className="h-[34px] w-[2px] bg-gray-200"></div>
+        <div className="flex gap-3">
+          <Toggle />
+          <img className="w-[24px] h-[24px]" src="images/icon-moon.svg" alt="" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
+
+function Searchbar() {
+  return (
+    <div className="flex">
+      <div className="relative w-full h-12"> 
+        <input placeholder="search" type="search" id="search-bar" className="flex justify-end items-center w-full h-full bg-[#f4f4f4] rounded-xl px-6 text-[#2d2d2d] font-bold shadow-base focus:outline-none focus:ring-2 focus:ring-[#a445ed]" />
+
+        <label htmlFor="search-bar" className="absolute right-0 top-0 h-full flex items-center justify-center px-6 cursor-pointer" >
+          <button className="h-5 w-5">
+            {/* Use a fixed size for the icon/image */}
+            <img className="w-full h-full" src="/images/icon-search.svg" alt="Search" />
+          </button>
+        </label>
+    
+      </div>
+    </div>
+  );
+}
+
+function Search_keyword() {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex flex-col justify-start gap-1">
+        <h1 className="text-3xl font-bold text-[#2d2d2d]">keyboard</h1>
+        <h3 className="text-xl text-[#a445ed]">/ˈkiːbɔːd/</h3>
+      </div>
+      <div>
+        <button><img className="w-12 h-12" src="/images/icon-play.svg" alt="" /></button>
+      </div>
+    </div>
+  );
+}
+
+function Noun() {
+  return (
+    <div className="flex flex-col gap-4 ">
+      <div className="flex gap-6 items-center">
+        <h2 className="text-lg font-bold text-[#2d2d2d]">noun</h2>
+        <div className="h-[2px] w-full bg-[#e9e9e9]"></div>
+      </div>
+      <div className="flex flex-col text-[#2d2d2d] gap-4 text-[15px]">
+        <h3 className="text-base text-[#757575]">Meaning</h3>
+        <div className="max-w-full ml-4">
+          <ul className="list-outside list-disc marker:text-[#8f19e8] text-start space-y-4">
+            <li>(etc.) A set of keys used to operate a typewriter, computer etc.</li>
+            <li>A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.</li>
+            <li>A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex text-base gap-6">
+        <h3 className="text-[#757575]">Synonyms</h3>
+        <p className="font-bold text-[#a445ed]">electronic keyboard</p>
+      </div>
+    </div>
+  );
+}
+
+function Verb() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-6 items-center">
+        <h2 className="text-lg font-bold text-[#2d2d2d]">verb</h2>
+        <div className="h-[2px] w-full bg-[#e9e9e9]"></div>
+      </div>
+      <div className="flex flex-col text-[#2d2d2d] gap-4 text-[15px]">
+        <h3 className="text-base text-[#757575]">Meaning</h3>
+        <div className="w-full ml-4">
+          <ul className="list-outside list-disc marker:text-[#8f19e8] text-start flex flex-col gap-4">
+            <li>To type on a computer keyboard.</li>
+            <span className="text-[#757575]">“Keyboarding is the part of this job I hate the most.”</span>
+          </ul>
+        </div>
+      </div>
+      <div className="mt-4 h-[2px] w-full bg-[#e9e9e9]"></div>
+    </div>
+  );
+}
+
+function Source() {
+  return (
+    <div className="flex flex-col text-sm gap-2">
+      <h3 className="underline decoration-[#757575] underline-offset-2 text-[#757575]">Source</h3>
+      <div className="flex text-[#2d2d2d] gap-2">
+       <a className="underline decoration-[#757575] underline-offset-2" href="http://https://en.wiktionary.org/wiki/keyboard" target="_blank">https://en.wiktionary.org/wiki/keyboard</a>
+       <img src="/images/icon-new-window.svg" alt="" />
+      </div>
+    </div>
+  );
+}
+
+function Dropdown() {
+  return (
+          <div className="flex gap-4">
+            <span className="text-[14px] font-bold whitespace-nowrap">Sans Serif</span>
+            <button>
+              <img className="w-full h-full" src="images/icon-arrow-down.svg" alt="" />
+            </button>            
+            <ul className="hidden">
+              <button><li>Sans Serif</li></button>
+              <button><li>Serif</li></button>
+              <button><li>Mono</li></button>
+            </ul>
+          </div>
+  );
+}
+
+
+function Toggle() {
+  return (
+    <div className="flex items-center">
+      <input type="checkbox" id="toggleSwitch" className="sr-only peer" />
+
+      <label
+        htmlFor="toggleSwitch"
+        className="
+          relative 
+          inline-flex 
+          items-center 
+          cursor-pointer 
+          w-[42px] 
+          h-[20px] 
+          rounded-full 
+          transition-colors 
+          duration-300
+          
+          bg-gray-500 
+          peer-checked:bg-indigo-600
+        "
+      >
+        <span
+          className="
+            absolute 
+            left-[4px] 
+            w-[14px]
+            h-[14px]
+            bg-white 
+            rounded-full 
+            shadow-md 
+            
+            transition duration-300   <-- Transition for the slide
+            transform                 <-- Enables the translate-x to work
+            
+            peer-checked:translate-x-full
+          "
+        ></span>
+      </label>
+    </div>
+  );
+}
+// interface Prop {
+//   onClick: MouseEventHandler<HTMLDivElement>;
+//   checked: boolean | undefined;
+//   className: string | undefined;
+// }
+// function Toggle({ onClick, checked, className }: Prop) {
+//   return (
+//     <div className={`relative ${className}`} onClick={onClick}>
+//       <input type="checkbox" checked={checked} readOnly className="sr-only" />
+//       <div className="block bg-grayCustom dark:bg-purpleCustom h-5 w-[2.08rem] rounded-full"></div>
+//       <div
+//         className={`absolute left-[0.20rem] top-[0.20rem] bg-white w-[0.85rem] h-[0.85rem] rounded-full transition
+//                 ${checked ? "translate-x-full" : "translate-x-0"}
+//                   `}
+//       ></div>
+//     </div>
+//   );
+// }
+
